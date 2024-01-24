4af5b144a9f5643890bdb1e411d52441631709c9
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-01-24 22:11:38 +0100

    fix after removing medication dosage hisotry

--- src/app/diseases/diseases.module.ts
+++ src/app/diseases/diseases.module.ts
@@ -6,7 +6,6 @@ import { MaterialModule } from '../material.module';
 import { DiseaseDetailsNavComponent } from './disease-details-nav/disease-details-nav.component';
 import { TreatmentDetailsComponent } from './treatments/treatment-details/treatment-details.component';
 import { TreatmentsHistoryComponent } from './treatments/treatments-history/treatments-history.component';
-import { MedicationDosageHistoryComponent } from './treatments/medications/medication-dosage-history/medication-dosage-history.component';
 
 const routes: Routes = [
   { path: '', component: DiseasesListComponent },
@@ -21,7 +20,6 @@ const routes: Routes = [
     DiseaseDetailsNavComponent,
     TreatmentDetailsComponent,
     TreatmentsHistoryComponent,
-    MedicationDosageHistoryComponent,
   ],
   imports: [
     MaterialModule,
