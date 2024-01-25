c872d21cbf9fbd47fbb060d6ccff6bc8af7ed291
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-01-25 21:24:06 +0100

    working on new medication form

---
+++ src/app/diseases/treatments/new-treatment/new-treatment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-treatment',
+  templateUrl: './new-treatment.component.html',
+  styleUrl: './new-treatment.component.scss'
+})
+export class NewTreatmentComponent {
+
+}
